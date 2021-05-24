6598429ad73b44ad2f93750e5182630a1d75de63
zjw <15766783508@163.com>
2021-05-24 23:07:22 +0800

    '测试回滚‘

--- rabbitmq_springboot/src/main/java/com/exe/direct/directCustomer.java
+++ rabbitmq_springboot/src/main/java/com/exe/direct/directCustomer.java
@@ -6,6 +6,9 @@ import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * 监听
+ */
 @Component
 public class directCustomer {
 
